3e5a3ce449d9ad8bc719bdaf73e300df9679068c
Alexandre L <alex131089@msn.com>
2016-12-10 04:52:24 +0100

    Py3 fix - auth

--- u2flib_host/authenticate.py
+++ u2flib_host/authenticate.py
@@ -103,16 +103,16 @@ def parse_args():
 def main():
     args = parse_args()
 
-    facet = text_type(args.facet, sys.stdin.encoding or sys.getdefaultencoding())
+    facet = args.facet
     if args.infile:
         with open(args.infile, 'r') as f:
             data = f.read()
     else:
         if sys.stdin.isatty():
             sys.stderr.write('Enter AuthenticateRequest JSON data...\n')
-        data = sys.stdin.read()
+        data = sys.stdin.readline()
 
-    params = json.loads(data, object_hook=u2str)
+    params = json.loads(data)
 
     if args.soft:
         from u2flib_host.soft import SoftU2FDevice
